c931ae2073eef044c5ca3c5ba402eab65174941a
Enrique Pinedo <enrjon@gmail.com>
2021-10-13 14:12:29 -0400

    functional model implementation

--- mnist_example.py
+++ mnist_example.py
@@ -4,7 +4,7 @@ import numpy as np
 from tensorflow.keras.layers import Conv2D, Input, Dense, MaxPool2D, BatchNormalization, GlobalAvgPool2D
 
 #tensorflow.keras.Sequential
-model = tensorflow.keras.Sequential(
+seq_model = tensorflow.keras.Sequential(
     [
         Input(shape=(28,28,1)),
         Conv2D(32, (3,3), activation='relu'),
@@ -24,6 +24,26 @@ model = tensorflow.keras.Sequential(
 )
 
 # functional approach : function that returns a model
+def functional_model():
+
+    my_input = Input(shape=(28,28,1))
+    x = Conv2D(32, (3,3), activation='relu')(my_input)
+    x = Conv2D(64, (3,3), activation='relu')(x)
+    x = MaxPool2D()(x)
+    x = BatchNormalization()(x)
+
+    x = Conv2D(128, (3,3), activation='relu')(x)
+    x = MaxPool2D()(x)
+    x = BatchNormalization()(x)
+
+    x = GlobalAvgPool2D()(x)
+    x = Dense(64, activation='relu')(x)
+    x = Dense(10, activation='softmax')(x)
+
+    model = tensorflow.keras.Model(inputs=my_input, outputs=x)
+
+    return model
+
 # tensorflow.keras.Model : inherit from this class
 
 
@@ -67,6 +87,7 @@ if __name__=='__main__':
     y_train = tensorflow.keras.utils.to_categorical(y_train, 10)
     y_test = tensorflow.keras.utils.to_categorical(y_test, 10)
 
+    model = functional_model()
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics='accuracy')
 
     # model training
