2e5cafd694bef396976244fb0d6bd256b84f0631
Enrique Pinedo <enrjon@gmail.com>
2021-10-13 16:00:55 -0400

    restructed into multiple pages

--- mnist_example.py
+++ mnist_example.py
@@ -2,6 +2,10 @@ import tensorflow
 import matplotlib.pyplot as plt
 import numpy as np
 from tensorflow.keras.layers import Conv2D, Input, Dense, MaxPool2D, BatchNormalization, GlobalAvgPool2D
+from tensorflow.python.keras import activations
+
+from deeplearning_models import functional_model, MyCustomModel
+from my_utils import display_some_examples
 
 #tensorflow.keras.Sequential
 seq_model = tensorflow.keras.Sequential(
@@ -22,83 +26,6 @@ seq_model = tensorflow.keras.Sequential(
 
     ]
 )
-
-# functional approach : function that returns a model
-def functional_model():
-
-    my_input = Input(shape=(28,28,1))
-    x = Conv2D(32, (3,3), activation='relu')(my_input)
-    x = Conv2D(64, (3,3), activation='relu')(x)
-    x = MaxPool2D()(x)
-    x = BatchNormalization()(x)
-
-    x = Conv2D(128, (3,3), activation='relu')(x)
-    x = MaxPool2D()(x)
-    x = BatchNormalization()(x)
-
-    x = GlobalAvgPool2D()(x)
-    x = Dense(64, activation='relu')(x)
-    x = Dense(10, activation='softmax')(x)
-
-    model = tensorflow.keras.Model(inputs=my_input, outputs=x)
-
-    return model
-
-# tensorflow.keras.Model : inherit from this class
-class MyCustomModel(tensorflow.keras.Model):
-
-    def __init__(self):
-        super().__init__()
-
-        self.conv1 = Conv2D(32, (3,3), activation='relu')
-        self.conv2 = Conv2D(64, (3,3), activation='relu')
-        self.maxpool1 = MaxPool2D()
-        self.batchnorm1 = BatchNormalization()
-
-        self.conv3 = Conv2D(128, (3,3), activation='relu')
-        self.maxpool2 = MaxPool2D()
-        self.batchnorm2 = BatchNormalization()
-
-        self.globalavgpool1 = GlobalAvgPool2D()
-        self.dense1 = Dense(64, activation='relu')
-        self.dense2 = Dense(10, activation='softmax')
-
-
-    def call(self, my_input):
-        
-        x = self.conv1(my_input) 
-        x = self.conv2(x)
-        x = self.maxpool1(x)
-        x = self.batchnorm1(x)
-
-        x = self.conv3(x)
-        x = self.maxpool2(x)
-        x = self.batchnorm2(x)
-
-        x = self.globalavgpool1(x)
-        x = self.dense1(x)
-        x = self.dense2(x)
-
-        return x
-
-
-def display_some_examples(examples, labels):
-
-    plt.figure(figsize=(10,10))
-
-    for i in range(25):
-
-        idx = np.random.randint(0, examples.shape[0]-1)
-        img = examples[idx]
-        label = labels[idx]
-
-        plt.subplot(5,5, i+1)
-        plt.title(str(label))
-        plt.tight_layout()
-        plt.imshow(img, cmap='gray')
-
-    plt.show()
-
 
 
 if __name__=='__main__':
@@ -131,3 +58,4 @@ if __name__=='__main__':
 
     # Evaluation on test set
     model.evaluate(x_test, y_test, batch_size=64)
+
